124687fb0be1eb5238eceb44f0348a5b0eb065d1
merin86 <m_liljenroth@hotmail.com>
2024-03-21 13:55:10 +0000

    Create superuser

--- husman_moderna/settings.py
+++ husman_moderna/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6%m=kfo7@)^&hazc#h)bu_er7088mwp!8z88%s3_9i4_myf=c!'
+SECRET_KEY = os.environ.get('v9eeabc&s6_t82&#b!ttdw0k=jf9ps7g9*4ap5lz%$9%w#2u$y')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -74,13 +78,16 @@ WSGI_APPLICATION = 'husman_moderna.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
