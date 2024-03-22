d942b54f5b102457e5c238272fc5f7e05878cb9f
merin86 <m_liljenroth@hotmail.com>
2024-03-22 13:16:47 +0000

    Register dish model in admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Dish
 
 # Register your models here.
+
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price')
+
+admin.site.register(Dish, DishAdmin)
