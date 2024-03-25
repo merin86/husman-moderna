7768ba71a944fa63f968f4ecb62c0534daa5f169
merin86 <m_liljenroth@hotmail.com>
2024-03-25 13:20:45 +0000

    Add categories in the model

--- menu/models.py
+++ menu/models.py
@@ -6,6 +6,12 @@ class Dish(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    CATEGORY_CHOICES = [
+        ('Appetizer', 'Appetizer'),
+        ('Main Course', 'Main Course'),
+        ('Dessert', 'Dessert'),
+    ]
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
 
     def __str__(self):
         return self.name
--- menu/views.py
+++ menu/views.py
@@ -4,7 +4,13 @@ from .models import Dish
 # Create your views here.
 
 def menu(request):
-    dishes = Dish.objects.all()
-    return render(request, 'menu.html', {'dishes': dishes})
-
-
+    appetizers = Dish.objects.filter(category='Appetizer')
+    main_courses = Dish.objects.filter(category='Main Course')
+    desserts = Dish.objects.filter(category='Dessert')
+    
+    context = {
+        'appetizers': appetizers,
+        'main_courses': main_courses,
+        'desserts': desserts,
+    }
+    return render(request, 'menu.html', context)
