f228522b3bda88fbd3a1b826619918a087393395
merin86 <m_liljenroth@hotmail.com>
2024-03-26 14:28:25 +0000

    Add links to navbar

--- husman_moderna/settings.py
+++ husman_moderna/settings.py
@@ -63,7 +63,9 @@ ROOT_URLCONF = 'husman_moderna.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
