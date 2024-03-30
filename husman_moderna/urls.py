b2bec7b8c766469475268572853602e1a255e667
merin86 <m_liljenroth@hotmail.com>
2024-03-30 17:18:50 +0000

    added authentication

--- husman_moderna/urls.py
+++ husman_moderna/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
     path('menu/', include('menu.urls')),
