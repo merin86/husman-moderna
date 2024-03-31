e0d8ced9090631588af8bc9b468a85848cec25f9
merin86 <m_liljenroth@hotmail.com>
2024-03-31 16:09:45 +0000

    Add styles to review page

--- reviews/forms.py
+++ reviews/forms.py
@@ -3,8 +3,8 @@ from .models import Review
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class ReviewForm(forms.ModelForm):
-    rating = forms.IntegerField(widget=forms.NumberInput(attrs={'min': '1', 'max': '10'}))
-    
+    rating = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'rating-input', 'min': '1', 'max': '10'}))
+
     class Meta:
         model = Review
         fields = ['title', 'content', 'rating']
