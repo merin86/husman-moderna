6c5b318d27c7de844dcfa42caa3fcb318c311037
merin86 <m_liljenroth@hotmail.com>
2024-04-09 09:12:44 +0000

    Create a test from models in about page

---
+++ about/tests/test_models.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from about.models import AboutPage
+
+# Test for the AboutPage model to ensure it behaves as expected
+class AboutPageModelTest(TestCase):
+
+    # Test the string representation of AboutPage
+    def test_string_representation(self):
+        about_page = AboutPage(content="Test About Page Content")
+        self.assertEqual(str(about_page), "About Page Content")
